0d8c148f4365eb1101ee08066064634b825cde25
j5rojo <j5rojo94@gmail.com>
2023-04-26 21:20:20 +0100

    defineComponent vs script setup

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,41 @@
+<!-- Old way <script lang="ts">
+import {defineComponent, ref} from "vue"
+
+export default defineComponent({
+    setup(props, {emit}) {
+
+        const counter = ref(0)
+
+        const increase = () => {
+            counter.value = counter.value + 1
+        }
+
+        return {
+            counter,
+            increase
+        }
+    }
+})
+</script>-->
+
+<!--Script setup way-->
 <script setup lang="ts">
+import {ref} from "vue"
+
+const counter = ref(0)
+const increase = () => counter.value += 1
 </script>
 
 <template>
-  <div class="wrapper">
-      <h1>hola</h1>
-      <p>Mundo</p>
-  </div>
+    <div class="wrapper">
+        <h1>Counter: {{ counter }}</h1>
+        <button @click="increase">+</button>
+    </div>
 </template>
 
 <style scoped>
-  .wrapper {
-      display: flex;
-      flex-direction: column;
-  }
+.wrapper {
+    display: flex;
+    flex-direction: column;
+}
 </style>
