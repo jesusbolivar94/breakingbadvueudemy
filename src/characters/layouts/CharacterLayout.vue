3510152b67b6879f2a42112e27d84c5e955df2e9
j5rojo <j5rojo94@gmail.com>
2023-05-02 22:08:14 +0100

    Adds a second navbar to the menu for children routes.

--- src/characters/layouts/CharacterLayout.vue
+++ src/characters/layouts/CharacterLayout.vue
@@ -1,11 +1,28 @@
 <script setup lang="ts">
+import {RouterView} from "vue-router"
+import NavBar from "@/shared/components/NavBar.vue"
+import type {RouterLink} from "@/router/link-routes"
+import {charactersRoute} from "@/characters/router"
+
+const routeLinks: RouterLink[] = charactersRoute.children!
+    .filter( router => (router.props as { visible: boolean }).visible  )
+    .map( route => {
+        return {
+            name: route.name as string,
+            path: route.path,
+            title: (route.props as { title:string, visible: boolean }).title
+        }
+    } )
 
 </script>
 
 <template>
-    <div>
-        <h1>CharacterLayout</h1>
-    </div>
+    <h1>Characters</h1>
+    <!-- Navbar -->
+    <NavBar :show-icon="false" :links="routeLinks" />
+
+    <!-- Routerview + Suspense -->
+    <RouterView/>
 </template>
 
 <style scoped>
--- src/characters/router/index.ts
+++ src/characters/router/index.ts
@@ -10,8 +10,23 @@ export const charactersRoute: RouteRecordRaw = {
     redirect: '/characters/list',
     component: CharacterLayout,
     children: [
-        { path: 'by/id', name: 'character-id', component: CharacterId },
-        { path: 'list', name: 'character-list', component: CharacterList },
-        { path: 'search', name: 'character-search', component: CharacterSearch },
+        {
+            path: 'by/id',
+            name: 'character-id',
+            props: { title: 'Characters per Id', visible: false, },
+            component: CharacterId
+        },
+        {
+            path: 'list',
+            name: 'character-list',
+            props: { title: 'List of Characters', visible: true, },
+            component: CharacterList
+        },
+        {
+            path: 'search',
+            name: 'character-search',
+            props: { title: 'Search Characters', visible: true, },
+            component: CharacterSearch
+        },
     ]
 }
